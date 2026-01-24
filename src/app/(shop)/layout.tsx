243c75a6e527c1f4e411320d594bda88be6c8245
willsrockson <marleyenos18@gmail.com>
2026-01-24 03:00:36 +0000

    Website url icons added

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -10,6 +10,8 @@ import FloatingSellButton from "@/components/sharedUi/FloatingSellButton";
 
 
 export const metadata: Metadata = {
+  manifest: '/site.webmanifest',
+  
   title: {
     default: "Tonmame Market Hub | Buy & Sell in Ghana",
     template: "%s | Tonmame Market Hub",
@@ -44,6 +46,17 @@ export const metadata: Metadata = {
     description: "The safest marketplace in Ghana.",
     images: ["/images/org.avif"],
   },
+   
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180' },
+    ],
+  },
+
 };
 
 
